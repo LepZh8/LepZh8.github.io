b034c74629050c27db6cd4f212122aeb633f21a3
Lep_Zh <lep_zhang@163.com>
2024-03-09 16:58:23 +0800

    Site updated: 2024-03-09 16:58:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/0.html"];function toRandomPost(){
+var posts=["posts/16107.html","posts/309.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
